aecdc0462ebeaac7a6835a2ba9563944b55e7b15
mapfap <2062638+mapfap@users.noreply.github.com>
2020-03-20 23:52:51 +0700

    Setup project

---
+++ src/main/java/com/code/i/IApplication.java
@@ -0,0 +1,13 @@
+package com.code.i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/i/IApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
